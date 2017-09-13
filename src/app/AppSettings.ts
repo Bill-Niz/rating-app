69e11b7bcafe3e5eba1f4aa2c76a1a00f1a44712
Bill Nizeyimana <billniz@me.com>
2017-09-13 17:19:00 +0200

    update to https

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,3 +1,3 @@
 export class AppSettings {
-  public static API_ENDPOINT= 'http://ec2-user@ec2-54-210-179-32.compute-1.amazonaws.com/api/v1';
+  public static API_ENDPOINT= 'https://ec2-user@ec2-54-210-179-32.compute-1.amazonaws.com/api/v1';
 }
