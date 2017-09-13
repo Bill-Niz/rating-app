a150f9c6b155c3d5f17c4b5fbd29f294e6681825
Bill Nizeyimana <billniz@me.com>
2017-09-13 18:51:40 +0200

    update server

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 80);
+app.set('port', process.env.PORT || 80);
+
+const server = app.listen(app.get('port'), function() {
+  console.log(' Server listening on port ' + server.address().port);
+});
